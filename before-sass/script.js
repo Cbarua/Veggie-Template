d7cc2f06daa1f82bb0a98612598f349414b1731f
Chinmoy Barua <cbaura19@gmail.com>
2022-01-22 16:10:12 +0530

    Fixed animation on load

--- before-sass/script.js
+++ before-sass/script.js
@@ -1,3 +1,4 @@
+document.querySelector('.nojs').classList.remove('nojs');
 const menuItemBtns = document.querySelectorAll('.group-item');
 const menuItemLists = document.querySelectorAll('.menu-items .row');
 
@@ -28,7 +29,6 @@ const observer1 = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('animate');
-            return;
         }
     });
 });
@@ -37,26 +37,19 @@ const observer2 = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('animate-children');
-            return;
         }
     });
 });
-
-banner.classList.remove('animate');
-contactForm.classList.remove('animate');
 
 observer1.observe(banner);
 observer1.observe(contactForm);
 
 items.forEach(e => {
-    e.classList.remove('animate-children');
     observer2.observe(e);
 });
 specialTexts.forEach(e => {
-    e.classList.remove('animate-children');
     observer2.observe(e);
 });
 aboutTexts.forEach(e => {
-    e.classList.remove('animate-children');
     observer2.observe(e);
 });
